b46128684a95183eb7b366c71374af4b0d1294f5
andrewemcmanus <60327690+andrewemcmanus@users.noreply.github.com>
2020-10-23 08:34:17 -0400

    10-23-20

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello, front end');
+//console.log('Hello, front end');
+var temp = document.querySelector('#tempf');
+function ctof(temp) {
+    var ctemp = (temp - 32) * 5/9;  
+    return ctemp;
+} 
+var text = ctof(temp);
+console.log(text);
+document.querySelector('#celsius').textContent = text;
+
+
+
